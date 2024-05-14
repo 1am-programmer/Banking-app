8541b23cb7e1d4344102c7dc9a9fa65fa896e376
Your Name <dannydotdev@gmail.com>
2024-05-14 15:15:03 +0100

    implement side bar with routing

---
+++ components/Sidebar.tsx
@@ -0,0 +1,58 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+import path from "path";
+
+const Sidebar = ({ user }: SiderbarProps) => {
+  const pathname = usePathname();
+
+  return (
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="mb-12 cursor-pointer items-center gap-2 ">
+          <Image
+            src="icons/logo.svg"
+            width="700"
+            height="700"
+            alt="image"
+            className="size-[24px] max-xl:size-14 "
+          />
+          <h1 className="horizon-logo">Horizon</h1>
+        </Link>
+
+        {sidebarLinks.map((item) => {
+          const isActive =
+            pathname === item.route || pathname.startsWith(`${item.route}/`);
+
+          return (
+            <Link
+              href={item.route}
+              key={item.label}
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
+            >
+              <div className="relative size-6">
+                <Image
+                  src={item.imgURL}
+                  alt="icons"
+                  fill
+                  className={cn({
+                    "brightness-[3] invert-0": isActive,
+                  })}
+                />
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {item.label}
+              </p>
+            </Link>
+          );
+        })}
+      </nav>
+    </section>
+  );
+};
+
+export default Sidebar;
