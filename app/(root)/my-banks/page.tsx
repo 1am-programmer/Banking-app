880b3c2bdffe82e54016424b62d868866e5e7efd
Your Name <dannydotdev@gmail.com>
2024-05-16 15:47:41 +0100

    complete layout

---
+++ app/(root)/my-banks/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>MY BANKS</div>;
+};
+
+export default page;
---
+++ app/(root)/transaction-history/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>TRANSACTION HISTORY</div>;
+};
+
+export default page;
