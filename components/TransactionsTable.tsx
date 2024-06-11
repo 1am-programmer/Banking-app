5d738a2e48aa55229b2d8f8cd2ab03078d2f5527
Your Name <dannydotdev@gmail.com>
2024-06-11 16:20:47 +0100

    Transactions data : status, amount, Credit/Debit and other valuable transaction info

--- components/TransactionsTable.tsx
+++ components/TransactionsTable.tsx
@@ -1,7 +1,43 @@
 import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { formatAmount, getTransactionStatus } from "@/lib/utils";
 
-const TransactionsTable = () => {
-  return <div>TransactionsTable</div>;
+const TransactionsTable = ({ transactions }: TransactionTableProps) => {
+  return (
+    <div>
+      <Table>
+        <TableHeader className="bg-[#f9fafb]">
+          <TableRow>
+            <TableHead className="px-2">Transactions</TableHead>
+            <TableHead className="px-2">Amount</TableHead>
+            <TableHead className="px-2">Status</TableHead>
+            <TableHead className="px-2">Date</TableHead>
+            <TableHead className="px-2  max-md:hidden">Channel</TableHead>
+            <TableHead className="px-2 max-md:hidden ">Category</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {transactions.map((transaction: Transaction) => {
+            const status = getTransactionStatus(new Date(transaction.date));
+            const amount = formatAmount(transaction.amount);
+
+            const isDebit = transaction.type === "debit";
+            const isCredit = transaction.type === "credit";
+
+            return <TableRow key={transaction.id}></TableRow>;
+          })}
+        </TableBody>
+      </Table>
+    </div>
+  );
 };
 
 export default TransactionsTable;
