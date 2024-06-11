a544a4a96b10d242be87b2e8b9c2258bfd300bf7
Your Name <dannydotdev@gmail.com>
2024-06-11 12:14:31 +0100

    integrated dwolla and plaid in user sign up form to connect bank accounts on account creation

--- unused/unused.js
+++ unused/unused.js
@@ -1,6 +1,7 @@
 /*
 https://cloud.appwrite.io/console/project-664857ee0034fd714bc5/auth
  https://dashboard.plaid.com/developers/keys
+ https://dashboard.plaid.com/overview
  https://plaid.com/docs/link/web/
  https://cloud.appwrite.io/console/project-665850d3001517ade180/overview/platforms
  https://appwrite.io/docs/tutorials/nextjs-ssr-auth/step-1
