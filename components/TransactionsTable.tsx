30ca84d9ecf97a9c9f769b5b39e9f6aad65f1fdb
Your Name <dannydotdev@gmail.com>
2024-06-12 10:27:44 +0100

    transaction table styling

--- components/TransactionsTable.tsx
+++ components/TransactionsTable.tsx
@@ -9,11 +9,21 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import {
+  cn,
   formatAmount,
   formatDateTime,
   getTransactionStatus,
   removeSpecialCharacters,
 } from "@/lib/utils";
+
+const CategoryBadge = ({ category }: CategoryBadgeProps) => {
+  return (
+    <div className={cn("category-badge")}>
+      <div className={cn("size-2 rounded-full")} />
+      <p className={cn("text-[12px] font-medium")}>{category}</p>
+    </div>
+  );
+};
 
 const TransactionsTable = ({ transactions }: TransactionTableProps) => {
   return (
@@ -38,23 +48,48 @@ const TransactionsTable = ({ transactions }: TransactionTableProps) => {
             const isCredit = transaction.type === "credit";
 
             return (
-              <TableRow key={transaction.id}>
-                <TableCell>
-                  <div>
-                    <h1>{removeSpecialCharacters(transaction.name)}</h1>
+              <TableRow
+                key={transaction.id}
+                className={`${
+                  isDebit || amount[0] === "-" ? "bg-[#fffbfa]" : "bg-[#f6fef9]"
+                } !over:bg-none !border-b-default `}
+              >
+                {/* Can be  250px */}
+                <TableCell className="max-w-[200px] pl-2 pr-10">
+                  <div className="flex items-center gap-3">
+                    <h1 className=" text-14  truncate font-semibold text-[#344054]">
+                      {removeSpecialCharacters(transaction.name)}
+                    </h1>
                   </div>
                 </TableCell>
-                <TableCell>
+
+                <TableCell
+                  className={`pr-2  pl-10  font-semibold
+                     ${
+                       isDebit || amount[0] === "-"
+                         ? "text-[#f04438]"
+                         : "text-[#039855]"
+                     }
+                  `}
+                >
                   {isDebit ? `-${amount}` : isCredit ? amount : amount}
                 </TableCell>
 
-                <TableCell>{status}</TableCell>
-                <TableCell>
+                <TableCell className=" pl-2 pr-10">
+                  <CategoryBadge category={status} />
+                </TableCell>
+
+                <TableCell className="min-w-32 pl-2 pr-10">
                   {formatDateTime(new Date(transaction.date)).dateTime}
                 </TableCell>
 
-                <TableCell>{transaction.paymentChannel}</TableCell>
-                <TableCell>{transaction.category}</TableCell>
+                <TableCell className=" pl-2 pr-10 capitalize min-w-24">
+                  {transaction.paymentChannel}
+                </TableCell>
+
+                <TableCell className=" pl-2 pr-10 max-md:hidden">
+                  <CategoryBadge category={transaction.category} />
+                </TableCell>
               </TableRow>
             );
           })}
@@ -63,5 +98,4 @@ const TransactionsTable = ({ transactions }: TransactionTableProps) => {
     </div>
   );
 };
-
 export default TransactionsTable;
